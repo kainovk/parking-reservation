7b71dfd67c31a9594ff4f065d57255b459c13827
Kirill <kaynov.w@gmail.com>
2021-12-04 15:24:07 +0300

    init

---
+++ src/main/java/ru/tinkoff/fintech/parking/Application.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.fintech.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
